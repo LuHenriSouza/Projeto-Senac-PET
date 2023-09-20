cdb547c977e4f61cddcb68a932f0f4401346f4a8
Luiz <luizhapsouza@gmail.com>
2023-09-19 22:03:03 -0300

    Começando o formulario de Pessoa

---
+++ resources/views/components/Forms/cadastro-animal.blade.php
@@ -0,0 +1,41 @@
+    <!-- Contact Start -->
+    <div class="container-fluid pt-5">
+        <div class="container">
+            <div class="border-start border-5 border-primary ps-5 mb-5" style="max-width: 600px;">
+                <h6 class="text-primary text-uppercase">Pessoa</h6>
+                <h1 class="display-5 text-uppercase mb-0">Complementar Cadastro</h1>
+            </div>
+            <div class="row g-5">
+                <div class="col-lg-7">
+                    <form action="">
+                        <div class="row g-3">
+                            <div class="col-12">
+                                <input type="text" class="form-control bg-light border-0 px-4" placeholder="Endereço"
+                                    style="height: 55px;">
+                            </div>
+                            <div class="col-12">
+                                <input type="text" class="form-control bg-light border-0 px-4" placeholder="CEP"
+                                    style="height: 55px;">
+                            </div>
+                            <div class="col-12">
+                                <input type="text" class="form-control bg-light border-0 px-4" placeholder="Cidade"
+                                    style="height: 55px;">
+                            </div>
+                            <div class="col-12">
+                                <input type="text" class="form-control bg-light border-0 px-4" placeholder="Estado"
+                                    style="height: 55px;">
+                            </div>
+                            <div class="col-12">
+                                <input type="email" class="form-control bg-light border-0 px-4" placeholder="Telefone"
+                                    style="height: 55px;">
+                            </div>
+                            <div class="col-12">
+                                <button class="btn btn-primary w-100 py-3" style="max-width: 200px;" type="submit">Próximo</button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Contact End -->
--- resources/views/projeto/dashboard.blade.php
+++ resources/views/projeto/dashboard.blade.php
@@ -1,3 +1,5 @@
 @extends('layouts.main')
 @section('content')
+    {{-- @include('components.Forms.cadastro-pessoa') --}}
+    @include('components.Forms.cadastro-pessoa-detalhado')
 @endsection
