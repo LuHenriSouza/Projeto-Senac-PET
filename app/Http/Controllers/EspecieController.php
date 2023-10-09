030a47a70e6eee47128015850ca9f11a2568f15a
gusantoro <gustavosantorodesousa@gmail.com>
2023-10-09 20:10:52 -0300

    bug

---
+++ app/Http/Controllers/EspecieController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Especie;
+use Illuminate\Http\Request;
+
+class EspecieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $especies = Especie::withCount('raca')->paginate(20);
+        return view('projeto.especies.especie-index')->with(compact('especies'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('projeto.especies.especie-create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Especie $especie)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Especie $especie)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Especie $especie)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Especie $especie)
+    {
+        //
+    }
+}
