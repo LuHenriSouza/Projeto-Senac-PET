b9e2cc84be97b6033056ed1d233bc69021908770
Luiz <luizhapsouza@gmail.com>
2023-10-09 22:19:46 -0300

    finalizado animais.create

--- resources/views/projeto/animais/animal-create.blade.php
+++ resources/views/projeto/animais/animal-create.blade.php
@@ -2,19 +2,3 @@
 @section('content')
     @include('components.Forms.cadastro-animal')
 @endsection
-@section('scripts')
-    <script>
-        function formatarData(input) {
-            var numero = input.value.replace(/\D/g, '');
-
-            if (numero.length >= 3) {
-                numero = numero.substring(0, 2) + '/' + numero.substring(2);
-            }
-            if (numero.length >= 6) {
-                numero = numero.substring(0, 5) + '/' + numero.substring(5);
-            }
-
-            input.value = numero;
-        }
-    </script>
-@endsection
